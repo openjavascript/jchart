8079642b5de792dcb5e3f58f16e3522aa6f88a65
openjavascript <openjavascript@btbtd.org>
2014-02-26 16:58:23 +0800

    Bizs.FormLogic add JC.Valid datavalid check support

--- docs_api/api.js
+++ docs_api/api.js
@@ -2,6 +2,7 @@ YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
         "JC.BaseMVC",
+        "JC.BaseMVC.Model",
         "JC.common",
         "JC.f",
         "JChart.Base",

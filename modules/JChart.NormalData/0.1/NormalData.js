f3ee91ba226a849d97a2aeeb58d557cb1e2eadad
openjavascript <openjavascript@btbtd.org>
2014-08-28 11:47:31 +0800

    1

--- modules/JChart.NormalData/0.1/NormalData.js
+++ modules/JChart.NormalData/0.1/NormalData.js
@@ -20,6 +20,40 @@
  */
 
     JChart.NormalData = {
+        /**
+         * 横向坐标数据
+         * @property    xAxis
+         * @type        {object}
+         */
+        xAxis: {
+            /**
+             * 横向坐标的显示数据
+             * @property    xAxis.categories
+             * @type        {Array of String}
+             * @default     null
+             * @example 
+             *  categories: [ 'cat1', 'cat2', 'cat3' ]
+             */
+            categories: null
+
+            /**
+             * 横向坐标显示数据是否自动换行 
+             * @property    xAxis.wordwrap
+             * @type        {Boolean}
+             * @default     true
+             * @example 
+             *  wordwrap: false
+             */
+            , wordwrap: true
+       }
+        /**
+         * 纵向坐标数据
+         * @property    yAxis
+         * @type        {object}
+         */
+        , yAxis: {
+       }
+
     };
 
     return NormalData;
